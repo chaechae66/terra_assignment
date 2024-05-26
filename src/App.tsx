a846ba64f7ca599e7b3d0d42f25d53926c996fb2
chaechae66 <valueworkerchae@gmail.com>
2024-05-26 21:48:18 +0900

    Chore: 툴팁 방향에 따른 버튼들 레이아웃 맞추기

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,52 @@ function App() {
   return (
     <>
       <div className="section">
-        <ToolTip>
-          <button className="button">TL</button>
-        </ToolTip>
+        <div className="top-section">
+          <ToolTip position={"TL"}>
+            <button className="button">TL</button>
+          </ToolTip>
+          <ToolTip position={"T"}>
+            <button className="button">Top</button>
+          </ToolTip>
+          <ToolTip position={"TR"}>
+            <button className="button">TR</button>
+          </ToolTip>
+        </div>
+        <div className="vertical-section">
+          <div className="left-section">
+            <ToolTip position={"LT"}>
+              <button className="button">LT</button>
+            </ToolTip>
+            <ToolTip position={"L"}>
+              <button className="button">Left</button>
+            </ToolTip>
+            <ToolTip position={"LB"}>
+              <button className="button">LB</button>
+            </ToolTip>
+          </div>
+          <div className="right-section">
+            <ToolTip position={"RT"}>
+              <button className="button">RT</button>
+            </ToolTip>
+            <ToolTip position={"R"}>
+              <button className="button">Right</button>
+            </ToolTip>
+            <ToolTip position={"RB"}>
+              <button className="button">RB</button>
+            </ToolTip>
+          </div>
+        </div>
+        <div className="bottom-section">
+          <ToolTip position={"BL"}>
+            <button className="button">BL</button>
+          </ToolTip>
+          <ToolTip position={"B"}>
+            <button className="button">Bottom</button>
+          </ToolTip>
+          <ToolTip position={"BR"}>
+            <button className="button">BR</button>
+          </ToolTip>
+        </div>
       </div>
     </>
   );
