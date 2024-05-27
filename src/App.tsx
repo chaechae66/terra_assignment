9d5c2436b4bc96839d3ce00168d1cb7428485276
chaechae66 <valueworkerchae@gmail.com>
2024-05-27 19:38:30 +0900

    Design: delay와 data-type 레이아웃 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -7,101 +7,582 @@ function App() {
       <section className="tooltip-section">
         <div className="section">
           <div className="top-section">
-            <ToolTip position={"TL"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"TL"}
+            >
               <button className="button">TL</button>
             </ToolTip>
-            <ToolTip position={"T"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"T"}
+            >
               <button className="button">Top</button>
             </ToolTip>
-            <ToolTip position={"TR"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"TR"}
+            >
               <button className="button">TR</button>
             </ToolTip>
           </div>
           <div className="vertical-section">
             <div className="left-section">
-              <ToolTip position={"LT"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"LT"}
+              >
                 <button className="button">LT</button>
               </ToolTip>
-              <ToolTip position={"L"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"L"}
+              >
                 <button className="button">Left</button>
               </ToolTip>
-              <ToolTip position={"LB"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"LB"}
+              >
                 <button className="button">LB</button>
               </ToolTip>
             </div>
             <div className="right-section">
-              <ToolTip position={"RT"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"RT"}
+              >
                 <button className="button">RT</button>
               </ToolTip>
-              <ToolTip position={"R"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"R"}
+              >
                 <button className="button">Right</button>
               </ToolTip>
-              <ToolTip position={"RB"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"RB"}
+              >
                 <button className="button">RB</button>
               </ToolTip>
             </div>
           </div>
           <div className="bottom-section">
-            <ToolTip position={"BL"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"BL"}
+            >
               <button className="button">BL</button>
             </ToolTip>
-            <ToolTip position={"B"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"B"}
+            >
               <button className="button">Bottom</button>
             </ToolTip>
-            <ToolTip position={"BR"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"BR"}
+            >
               <button className="button">BR</button>
             </ToolTip>
           </div>
         </div>
         <div className="overflow-section">
           <div className="top-section">
-            <ToolTip position={"TL"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"TL"}
+            >
               <button className="button">TL</button>
             </ToolTip>
-            <ToolTip position={"T"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"T"}
+            >
               <button className="button">Top</button>
             </ToolTip>
-            <ToolTip position={"TR"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"TR"}
+            >
               <button className="button">TR</button>
             </ToolTip>
           </div>
           <div className="vertical-section">
             <div className="left-section">
-              <ToolTip position={"LT"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"LT"}
+              >
                 <button className="button">LT</button>
               </ToolTip>
-              <ToolTip position={"L"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"L"}
+              >
                 <button className="button">Left</button>
               </ToolTip>
-              <ToolTip position={"LB"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"LB"}
+              >
                 <button className="button">LB</button>
               </ToolTip>
             </div>
             <div className="right-section">
-              <ToolTip position={"RT"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"RT"}
+              >
                 <button className="button">RT</button>
               </ToolTip>
-              <ToolTip position={"R"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"R"}
+              >
                 <button className="button">Right</button>
               </ToolTip>
-              <ToolTip position={"RB"}>
+              <ToolTip
+                message={
+                  <>
+                    prompt text
+                    <br />
+                    prompt text
+                    <br />
+                    prompt text
+                  </>
+                }
+                position={"RB"}
+              >
                 <button className="button">RB</button>
               </ToolTip>
             </div>
           </div>
           <div className="bottom-section">
-            <ToolTip position={"BL"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"BL"}
+            >
               <button className="button">BL</button>
             </ToolTip>
-            <ToolTip position={"B"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"B"}
+            >
               <button className="button">Bottom</button>
             </ToolTip>
-            <ToolTip position={"BR"}>
+            <ToolTip
+              message={
+                <>
+                  prompt text
+                  <br />
+                  prompt text
+                  <br />
+                  prompt text
+                </>
+              }
+              position={"BR"}
+            >
               <button className="button">BR</button>
             </ToolTip>
           </div>
         </div>
       </section>
+      <div className="delay-data-type-wrap">
+        <section className="delay-section">
+          <ToolTip
+            message={
+              <>
+                prompt text
+                <br />
+                prompt text
+                <br />
+                prompt text
+              </>
+            }
+            position="TL"
+          >
+            <button className="button">enter-delay 1s</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <>
+                prompt text
+                <br />
+                prompt text
+                <br />
+                prompt text
+              </>
+            }
+            position="T"
+          >
+            <button className="button">leave-delay 1s</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <>
+                prompt text
+                <br />
+                prompt text
+                <br />
+                prompt text
+              </>
+            }
+            position="TR"
+          >
+            <button className="button">enter delay 1s</button>
+          </ToolTip>
+        </section>
+        <section className="data-type-section">
+          <ToolTip message="string" position="TL">
+            <button className="button">string</button>
+          </ToolTip>
+          <ToolTip message={123} position="T">
+            <button className="button">number</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <>
+                <svg
+                  viewBox="64 64 896 896"
+                  focusable="false"
+                  data-icon="exclamation-circle"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                </svg>
+              </>
+            }
+            position="TR"
+          >
+            <button className="button">icon</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <>
+                <svg
+                  viewBox="64 64 896 896"
+                  focusable="false"
+                  data-icon="exclamation-circle"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                </svg>
+                <span>주의</span>
+              </>
+            }
+            position="B"
+          >
+            <button className="button">icon & string</button>
+          </ToolTip>
+        </section>
+        <section className="button-section">
+          <ToolTip
+            message={
+              <div className="message-outer">
+                <div className="message-inner">
+                  <svg
+                    viewBox="64 64 896 896"
+                    focusable="false"
+                    data-icon="exclamation-circle"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                  </svg>
+                  <span>Are you sure to delete this task?</span>
+                </div>
+                <button
+                  onClick={() => {
+                    alert("Top tooltip");
+                  }}
+                >
+                  Yes
+                </button>
+              </div>
+            }
+            position="T"
+          >
+            <button className="button">클릭 가능한 button</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <div className="message-outer">
+                <div className="message-inner">
+                  <svg
+                    viewBox="64 64 896 896"
+                    focusable="false"
+                    data-icon="exclamation-circle"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                  </svg>
+                  <span>Are you sure to delete this task?</span>
+                </div>
+                <button
+                  onClick={() => {
+                    alert("Left tooltip");
+                  }}
+                >
+                  Yes
+                </button>
+              </div>
+            }
+            position="L"
+          >
+            <button className="button">클릭 가능한 button</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <div className="message-outer">
+                <div className="message-inner">
+                  <svg
+                    viewBox="64 64 896 896"
+                    focusable="false"
+                    data-icon="exclamation-circle"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                  </svg>
+                  <span>Are you sure to delete this task?</span>
+                </div>
+                <button
+                  onClick={() => {
+                    alert("Right tooltip");
+                  }}
+                >
+                  Yes
+                </button>
+              </div>
+            }
+            position="R"
+          >
+            <button className="button">클릭 가능한 button</button>
+          </ToolTip>
+          <ToolTip
+            message={
+              <div className="message-outer">
+                <div className="message-inner">
+                  <svg
+                    viewBox="64 64 896 896"
+                    focusable="false"
+                    data-icon="exclamation-circle"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"></path>
+                  </svg>
+                  <span>Are you sure to delete this task?</span>
+                </div>
+                <button
+                  onClick={() => {
+                    alert("Bottom tooltip");
+                  }}
+                >
+                  Yes
+                </button>
+              </div>
+            }
+            position="B"
+          >
+            <button className="button">클릭 가능한 button</button>
+          </ToolTip>
+        </section>
+      </div>
     </>
   );
 }
