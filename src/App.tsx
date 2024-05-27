71535f23a5204c6aff537d0377da21d6aa6f4de0
chaechae66 <valueworkerchae@gmail.com>
2024-05-27 13:57:30 +0900

    Design: overflow일 때 레이아웃 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,104 @@ import { ToolTip } from "./components/ToolTip/ToolTip";
 function App() {
   return (
     <>
-      <div className="section">
-        <div className="top-section">
-          <ToolTip position={"TL"}>
-            <button className="button">TL</button>
-          </ToolTip>
-          <ToolTip position={"T"}>
-            <button className="button">Top</button>
-          </ToolTip>
-          <ToolTip position={"TR"}>
-            <button className="button">TR</button>
-          </ToolTip>
-        </div>
-        <div className="vertical-section">
-          <div className="left-section">
-            <ToolTip position={"LT"}>
-              <button className="button">LT</button>
+      <section className="tooltip-section">
+        <div className="section">
+          <div className="top-section">
+            <ToolTip position={"TL"}>
+              <button className="button">TL</button>
             </ToolTip>
-            <ToolTip position={"L"}>
-              <button className="button">Left</button>
+            <ToolTip position={"T"}>
+              <button className="button">Top</button>
             </ToolTip>
-            <ToolTip position={"LB"}>
-              <button className="button">LB</button>
+            <ToolTip position={"TR"}>
+              <button className="button">TR</button>
             </ToolTip>
           </div>
-          <div className="right-section">
-            <ToolTip position={"RT"}>
-              <button className="button">RT</button>
+          <div className="vertical-section">
+            <div className="left-section">
+              <ToolTip position={"LT"}>
+                <button className="button">LT</button>
+              </ToolTip>
+              <ToolTip position={"L"}>
+                <button className="button">Left</button>
+              </ToolTip>
+              <ToolTip position={"LB"}>
+                <button className="button">LB</button>
+              </ToolTip>
+            </div>
+            <div className="right-section">
+              <ToolTip position={"RT"}>
+                <button className="button">RT</button>
+              </ToolTip>
+              <ToolTip position={"R"}>
+                <button className="button">Right</button>
+              </ToolTip>
+              <ToolTip position={"RB"}>
+                <button className="button">RB</button>
+              </ToolTip>
+            </div>
+          </div>
+          <div className="bottom-section">
+            <ToolTip position={"BL"}>
+              <button className="button">BL</button>
             </ToolTip>
-            <ToolTip position={"R"}>
-              <button className="button">Right</button>
+            <ToolTip position={"B"}>
+              <button className="button">Bottom</button>
             </ToolTip>
-            <ToolTip position={"RB"}>
-              <button className="button">RB</button>
+            <ToolTip position={"BR"}>
+              <button className="button">BR</button>
             </ToolTip>
           </div>
         </div>
-        <div className="bottom-section">
-          <ToolTip position={"BL"}>
-            <button className="button">BL</button>
-          </ToolTip>
-          <ToolTip position={"B"}>
-            <button className="button">Bottom</button>
-          </ToolTip>
-          <ToolTip position={"BR"}>
-            <button className="button">BR</button>
-          </ToolTip>
+        <div className="overflow-section">
+          <div className="top-section">
+            <ToolTip position={"TL"}>
+              <button className="button">TL</button>
+            </ToolTip>
+            <ToolTip position={"T"}>
+              <button className="button">Top</button>
+            </ToolTip>
+            <ToolTip position={"TR"}>
+              <button className="button">TR</button>
+            </ToolTip>
+          </div>
+          <div className="vertical-section">
+            <div className="left-section">
+              <ToolTip position={"LT"}>
+                <button className="button">LT</button>
+              </ToolTip>
+              <ToolTip position={"L"}>
+                <button className="button">Left</button>
+              </ToolTip>
+              <ToolTip position={"LB"}>
+                <button className="button">LB</button>
+              </ToolTip>
+            </div>
+            <div className="right-section">
+              <ToolTip position={"RT"}>
+                <button className="button">RT</button>
+              </ToolTip>
+              <ToolTip position={"R"}>
+                <button className="button">Right</button>
+              </ToolTip>
+              <ToolTip position={"RB"}>
+                <button className="button">RB</button>
+              </ToolTip>
+            </div>
+          </div>
+          <div className="bottom-section">
+            <ToolTip position={"BL"}>
+              <button className="button">BL</button>
+            </ToolTip>
+            <ToolTip position={"B"}>
+              <button className="button">Bottom</button>
+            </ToolTip>
+            <ToolTip position={"BR"}>
+              <button className="button">BR</button>
+            </ToolTip>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
